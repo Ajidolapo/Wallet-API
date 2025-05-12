8b9c8fe6fd32b786a1aa18586a7c4d8ae57e0b01
0layimika <110624406+0layimika@users.noreply.github.com>
2025-05-12 08:53:56 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 const app = require('../app')
-const serverless = require("serverless-http");
-module.exports = serverless(app);
+// const serverless = require("serverless-http");
+module.exports = app;
